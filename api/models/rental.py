8cb3ebe0fc414d7e79128678b7fd4bbab0370c82
Job Matheka <job.matheka.m@gmail.com>
2020-12-09 21:05:19 +0300

    model bug fix

--- api/models/rental.py
+++ api/models/rental.py
@@ -12,7 +12,7 @@ class Rental(db.Model):
     __tablename__ = "rentals"
 
     id = db.Column(db.Integer, primary_key=True)
-    book_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
     due_at = db.Column(db.DateTime, nullable=True)
     status = db.Column(db.String(80), default=PENDING, nullable=False)
